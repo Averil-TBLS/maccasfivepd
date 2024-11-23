0fd54133212abee502237c95657e0cb389ff9f36
Averil-TBLS <averil@thinblueline.studio>
2024-11-23 02:10:50 -0800

    Create Outfit_PD.lua

---
+++ esx/adjustment/Outfit_PD.lua
@@ -0,0 +1,126 @@
+Config.Uniforms = {
+	recruit = {
+		male = {
+			tshirt_1 = 225,  tshirt_2 = 0,
+			torso_1 = 190,   torso_2 = 3,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 37,
+			pants_1 = 25,   pants_2 = 2,
+			shoes_1 = 51,   shoes_2 = 0,
+			helmet_1 = 3,  helmet_2 = 0,
+			chain_1 = 198,    chain_2 = 0,
+			ears_1 = -1,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 36,  tshirt_2 = 1,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = 45,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	officer = {
+		male = {
+			tshirt_1 = 225,  tshirt_2 = 0,
+			torso_1 = 190,   torso_2 = 3,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 37,
+			bag = 52,
+			pants_1 = 25,   pants_2 = 2,
+			shoes_1 = 51,   shoes_2 = 0,
+			helmet_1 = 3,  helmet_2 = 0,
+			chain_1 = 198,    chain_2 = 0,
+			ears_1 = -1,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	sergeant = {
+		male = {
+			tshirt_1 = 225,  tshirt_2 = 0,
+			torso_1 = 190,   torso_2 = 3,
+			decals_1 = 15,   decals_2 = 2,
+			arms = 37,
+			pants_1 = 25,   pants_2 = 2,
+			shoes_1 = 51,   shoes_2 = 0,
+			helmet_1 = 3,  helmet_2 = 0,
+			chain_1 = 198,    chain_2 = 0,
+			ears_1 = -1,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 1,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	lieutenant = {
+		male = {
+			tshirt_1 = 229,  tshirt_2 = 0,
+			torso_1 = 190,   torso_2 = 5,
+			decals_1 = 44,   decals_2 = 6,
+			arms = 37,
+			pants_1 = 25,   pants_2 = 2,
+			shoes_1 = 51,   shoes_2 = 0,
+			helmet_1 = 3,  helmet_2 = 0,
+			chain_1 = 198,    chain_2 = 0,
+			ears_1 = -1,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 2,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	boss = {
+		male = {
+			tshirt_1 = 229,  tshirt_2 = 0,
+			torso_1 = 190,   torso_2 = 5,
+			decals_1 = 44,   decals_2 = 7,
+			arms = 37,
+			pants_1 = 25,   pants_2 = 2,
+			shoes_1 = 51,   shoes_2 = 0,
+			helmet_1 = 3,  helmet_2 = 0,
+			chain_1 = 198,    chain_2 = 0,
+			ears_1 = -1,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 3,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
